5396c3fa816e4d58c7455425fed1da2f5c47a929
DJVERTER <creativekira1982@gmail.com>
2021-11-18 20:18:41 +0200

    add list of Git operations

--- test.java
+++ test.java
@@ -1,3 +1,6 @@
-fils fgty bada shmada vorada, tfgh, git reset
+1:Git status-actual status Git
+2:Git commit -m"comment"-commit changed
+3: Git push-upload on GitHub the changed items
+4: Git reset,soft, mixed, hard(~1)-reset to desired changes 
 
 
